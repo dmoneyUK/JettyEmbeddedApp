7587e6fbb4fcfbd7c670ebf24db8c161d4289b57
jinge.dai <jinge.dai@gamesys.co.uk>
2018-03-16 09:18:20 +0000

    UsingHandler created a hello handler

--- src/main/java/my/codebank/jetty/server/HelloHandler.java
+++ src/main/java/my/codebank/jetty/server/HelloHandler.java
@@ -1,25 +1,46 @@
 package my.codebank.jetty.server;
 
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.server.handler.ContextHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class HelloHandler extends AbstractHandler{
+public class HelloHandler extends AbstractHandler {
+    final String greeting;
+    final String body;
+
+    public HelloHandler() {
+        this("Handlers greeting");
+    }
+
+    public HelloHandler(String greeting) {
+        this(greeting, null);
+    }
+
+    public HelloHandler(String greeting, String body) {
+        this.greeting = greeting;
+        this.body = body;
+    }
+
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
-                       HttpServletResponse response)
-            throws IOException, ServletException
-    {
-        response.setContentType("text/html;charset=utf-8");
+                       HttpServletResponse response) throws IOException,
+            ServletException {
+        response.setContentType("text/html; charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>" + greeting + "</h1>");
+        if (body != null) {
+            out.println(body);
+        }
+
         baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
     }
 }
--- src/main/java/my/codebank/jetty/server/MyJettyServer.java
+++ src/main/java/my/codebank/jetty/server/MyJettyServer.java
@@ -2,8 +2,14 @@ package my.codebank.jetty.server;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.lf5.Log4JLogRecord;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.NCSARequestLog;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.RequestLogHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.log.Log;
@@ -17,32 +23,7 @@ public class MyJettyServer {
     public static void main(String[] args) throws Exception
     {
         Server server = new Server(8080);
-
-        /* User handler */
-        //ContextHandler context = new ContextHandler();
-        //context.setContextPath("/hello");
-        //context.setResourceBase(".");
-        //context.setClassLoader(Thread.currentThread().getContextClassLoader());
-        //server.setHandler(context);
-        //context.setHandler(new HelloHandler());
-
-        /* Use Servlet Context*/
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new HelloServlet()), "/*");
-        context.addServlet(new ServletHolder(new HelloServlet("Buongiorno Mondo")),"/it/*");
-        context.addServlet(new ServletHolder(new HelloServlet("Bonjour le Monde")),"/fr/*");
-
-        /* Use Application Context*/
-        String jetty_home = System.getProperty("jetty.base","..");
-
-        logger.info(jetty_home);
-        //WebAppContext webapp = new WebAppContext();
-        //webapp.setContextPath("/");
-        //webapp.setWar(jetty_home+"/webapps/test.war");
-        //server.setHandler(webapp);
-
+        server.setHandler(new HelloHandler());
         server.start();
         server.join();
     }
